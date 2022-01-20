d48aa0c0730a268d7959ac33da00e9b7ea483525
Fernando E. Silva Jacquier <fesilvajacquier@protonmail.com>
2022-01-20 15:45:31 -0300

    store.js: remove quantity selector from product show

--- store.js
+++ store.js
@@ -1,6 +1,17 @@
 (function(){
   console.log("Hello from the testing Store script");
-  const quantitySelector = document.querySelector("div[data-component='line-item.quantity']");
-  quantitySelector.remove();
-  console.log(LS.cart)
+  console.log(LS);
+
+  const quantitySelectors = document.querySelectorAll("div[data-component='line-item.quantity']");
+  if (quantitySelectors.length > 0) {
+    quantitySelectors.forEach((quantitySelector) => {
+      quantitySelector.remove();
+    })
+  }
+
+
+  const productShowQuantitySelector = document.querySelector("div[data-component='product.adding-amount']");
+  if (productShowQuantitySelector) {
+    productShowQuantitySelector.parentElement.remove();
+  }
 })();
