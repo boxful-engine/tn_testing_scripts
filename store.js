abecbba7caa129f2086f12319ca2b0108090f8df
Fernando E. Silva Jacquier <fesilvajacquier@protonmail.com>
2022-01-20 15:15:04 -0300

    Add store.js initial interactions

--- store.js
+++ store.js
@@ -1,3 +1,6 @@
 (function(){
   console.log("Hello from the testing Store script");
+  const quantitySelector = document.querySelector("div[data-component='line-item.quantity']");
+  quantitySelector.remove();
+  console.log(LS.cart)
 })();
